3a4dfcfd78eb5cc7afb54a88191d670dfa0dd938
Oscar Soto <osoto@sdi.es>
2020-02-12 13:03:33 +0100

    [REF] no 12.0

--- activities_sync_event/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import mail_activity
-from . import calendar_event
-from . import crm_lead
-from . import res_partner
-
--- opportunity_lost/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-# SDI
-# author: djuaneda
-
-
-from . import wizard
-from . import models
-
