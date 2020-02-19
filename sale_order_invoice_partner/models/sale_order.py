891592598e8c2fc3cc6f6888f99aa30d0c719aac
Oscar Soto <osoto@sdi.es>
2020-02-19 18:53:30 +0100

    [ADD][sale_order_invoice_partner]

---
+++ sale_order_invoice_partner/models/sale_order.py
@@ -0,0 +1,15 @@
+###############################################################################
+# For copyright and license notices, see __manifest__.py file in root directory
+###############################################################################
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        res = super(SaleOrder, self).onchange_partner_id()
+        if self.partner_id and self.partner_invoice_id.type != 'invoice':
+            self.partner_invoice_id = self.partner_id.commercial_partner_id.id
+        return res
