e9449de9bf090d6c16acbe5093aae937279fe58b
JorgeQuinteros <quinterosorellana04@gmail.com>
2020-02-28 11:19:48 +0100

    [Mr. Clean] remove import no used

--- contract_contract_invoice_partner/models/sale_order.py
+++ contract_contract_invoice_partner/models/sale_order.py
@@ -1,7 +1,7 @@
 ###############################################################################
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
