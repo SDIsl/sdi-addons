78cb6414282de139339348b1f3db7e52f48c9461
Sergio Lop Sanz <slop@sdi.es>
2021-05-27 09:58:31 +0200

    [IMP] Fix partner terms when SO hasn't them

---
+++ sale_order_partner_terms/__manifest__.py
@@ -0,0 +1,32 @@
+###############################################################################
+#
+#    SDi Soluciones Informáticas
+#    Copyright (C) 2021-Today SDi Soluciones Informáticas <www.sdi.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Sale Order Invoice Partner',
+    'summary': 'Change the invoice partner in the sale order, '
+    'to select the company or the invoice address of the partner.',
+    'category': 'Sales',
+    'version': '12.0.1.0.0',
+    'author': 'Sergio Lop, SDi Soluciones Informáticas',
+    'website': 'https://www.sdi.es/odoo/',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_management',
+    ],
+}
