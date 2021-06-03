55c9f3fe33044ed40bf45990c34dbd9be63df2d8
Sergio Lop Sanz <slop@sdi.es>
2021-06-03 10:45:49 +0200

    [NEW] Holiday days to next year

--- account_payment_term_extension_propagate/models/account_payment_term.py
+++ account_payment_term_extension_propagate/models/account_payment_term.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from dateutil.relativedelta import relativedelta
-from odoo import api, exceptions, fields, models
+from odoo import fields, models
 
 
 class AccountPaymentTermHoliday(models.Model):
