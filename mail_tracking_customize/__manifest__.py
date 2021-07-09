55a466d048bda61757d5a0c140e546c57da138f1
JorgeQuinteros <quinterosorellana04@gmail.com>
2021-07-09 12:56:44 +0200

    [ADD] mail_tracking_customize

---
+++ mail_tracking_customize/__manifest__.py
@@ -0,0 +1,36 @@
+###############################################################################
+#
+#    SDi Soluciones Digitales
+#    Copyright (C) 2021-Today SDi Soluciones Digitales <www.sdi.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Mail Tracking Customize',
+    'summary': '''Add the posibility of change tracking ID key to
+        avoid a flag in SpamAssassin''',
+    'author': 'Jorge Luis Quinteros, SDi Soluciones Digitales',
+    'website': 'https://sdi.web.sdi.es/odoo/',
+    'license': 'AGPL-3',
+    'category': 'Social',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'mail',
+        'mail_tracking',
+    ],
+    'data': [
+        # 'views/ir_mail_server_views.xml',
+    ],
+}
