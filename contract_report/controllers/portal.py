6e7e5e0ea3957146e3b8f8302db5685d723a2197
JorgeQuinteros <quinterosorellana04@gmail.com>
2021-10-05 18:34:45 +0200

    [IMP] PEP8

--- contract_report/controllers/portal.py
+++ contract_report/controllers/portal.py
@@ -6,7 +6,8 @@ from odoo import fields, http, _
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
 from odoo.addons.portal.controllers.mail import _message_post_helper
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as \
+    get_records_pager
 from odoo.osv import expression
 
 
