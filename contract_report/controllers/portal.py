0df2f8f9ad7156d69083f54a1713319e295d23bb
JorgeQuinteros <quinterosorellana04@gmail.com>
2021-11-24 18:22:14 +0100

    [Flake8]

--- contract_report/controllers/portal.py
+++ contract_report/controllers/portal.py
@@ -6,8 +6,8 @@ from odoo import fields, http, _
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
 from odoo.addons.portal.controllers.mail import _message_post_helper
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager as \
-    portal_pager, get_records_pager
+from odoo.addons.portal.controllers.portal import (CustomerPortal,
+                                                   get_records_pager)
 from odoo.osv import expression
 
 
