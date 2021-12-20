bec18332eabf89f8878b69dbe0dcbe25c69fbeff
valentincastravete <valentincastravete@gmail.com>
2021-12-20 12:22:33 +0100

    [ADD IMP] Resource and Employee changes

--- resource_management/models/expendable_resource.py
+++ resource_management/models/expendable_resource.py
@@ -24,13 +24,28 @@ class ExpendableResource(models.Model):
         string="Description",
     )
 
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        msg = _('<p><span class="fa fa-plus" title="New"/> '
+                'Resource <strong>{name}</strong> has been created:</br>'
+                '<li><strong>Name: </strong>{name}</li>'
+                ).format(name=res.name)
+        if res.description:
+            msg += _('<li><strong>Description: </strong>{description}</li></p>'
+                     ).format(description=res.description)
+        if res.delivery_date:
+            msg += _('<li><strong>Delivery Date: </strong>{delivery_date}</li>'
+                     '</p>').format(delivery_date=res.delivery_date)
+        res.employee_id.message_post(body=msg)
+        return res
+
     @api.multi
     def write(self, vals):
         for resource in self:
-            msg = _('<p>Changes in Expendable Resource '
-                    '<strong>{resource}</strong>:</br>').format(
-                resource=resource.name,
-            )
+            msg = _('<p><span class="fa fa-pencil" title="Edited"/> Resource '
+                    '<strong>{resource}</strong> has been modified:</br>'
+                    ).format(resource=resource.name,)
             post = False
             if 'name' in vals and vals.get(
                'name') != resource.name:
@@ -58,10 +73,11 @@ class ExpendableResource(models.Model):
     @api.multi
     def unlink(self):
         for resource in self:
-            msg = _('<p>An Expendable Resource was deleted:</p>'
-                    '<li><strong>Name: </strong>{resource}</li>'
-                    '<li><strong>Delivery Date: </strong>{delivery_date}</li>'
+            msg = _('<p><span class="fa fa-times" title="Deleted"/> '
+                    'Resource <strong>{resource}</strong> has been deleted:'
+                    '</p><li><strong>Name: </strong>{resource}</li>'
                     '<li><strong>Description: </strong>{description}</li>'
+                    '<li><strong>Delivery Date: </strong>{delivery_date}</li>'
                     ).format(
                 resource=resource.name, delivery_date=resource.delivery_date,
                 description=resource.description
