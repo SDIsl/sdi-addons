5985a283debc8ccc3509e57f972f335b3ccd168f
Sergio Lop Sanz <slop@sdi.es>
2022-03-09 17:46:32 +0100

    [ADD] Módulo dígito control secuencias

---
+++ ir_sequence_control_digit/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "IR Sequence control digits",
+
+    'summary': """
+        Gives you option to suffix a control digit 
+        """,
+    'author': "Sergio Lop <SDi Digital Group>",
+    'website': "https://www.sdi.es/odoo",
+    'category': 'base',
+    'version': '12.0.0.1',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+    ],
+}
---
+++ ir_sequence_control_digit/models/ir_sequence.py
@@ -0,0 +1,23 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class IrSequence(models.Model):
+    _inherit = 'ir.sequence'
+
+    control_digit = fields.Selection(
+        selection=[
+            ('10', "Mod 10")
+        ],
+        string="Control digit method",
+    )
+
+    def _next_do(self):
+        res = super(IrSequence, self)._next_do()
+        if self.control_digit == '10':
+            checksum = 0
+            for i, digit in enumerate(reversed(res)):
+                checksum += int(digit) * 3 if (i % 2 == 0) else int(digit)
+            res = '%s%d' % (res, (10 - (checksum % 10)) % 10)
+        return res
