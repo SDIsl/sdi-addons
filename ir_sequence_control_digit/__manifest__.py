c0b5b893178db38e144345b4a4f3b2ed178d5697
Sergio Lop Sanz <slop@sdi.es>
2022-03-10 10:03:47 +0100

    Update ir_sequence_control_digit/__manifest__.py
    Co-authored-by: Jorge Luis Quinteros <quinterosorellana04@gmail.com>

--- ir_sequence_control_digit/__manifest__.py
+++ ir_sequence_control_digit/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "IR Sequence control digits",
+    'name': 'IR Sequence control digits',
     'summary': 'This module gives you option to suffix a control digit',
     'author': "Sergio Lop <SDi Digital Group>",
     'website': "https://www.sdi.es/odoo",
