68951e68985a30f801a09d96bb99fbecd4500495
Jeggred <dam17.2021@gmail.com>
2022-04-19 10:15:24 +0200

    Nombre de proyecto en el asunto del mail

---
+++ project_name_mail_notification/models/res_partner.py
@@ -0,0 +1,30 @@
+from odoo import models, api
+
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def _notify(self, message, rdata, record, force_send=False,
+                send_after_commit=True, model_description=False,
+                mail_auto_delete=True):
+
+        projectpick = self.env['project.task'].search(
+            [('name', 'ilike', message.record_name)])
+
+        for records in projectpick:
+            if records:
+                message.subject = message.subject or (
+                    message.record_name and 'Re: [%s] %s' % (
+                        record.project_id.name, message.record_name
+                    )
+                )
+            else:
+                message.subject = message.subject or (
+                    message.record_name and 'Re: %s' % message.record_name
+                )
+
+        return super()._notify(message, rdata, record, force_send,
+                               send_after_commit, model_description,
+                               mail_auto_delete
+                               )
