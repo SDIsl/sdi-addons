05334b766aa1310a745cb99b29b060e7d03b56b0
Jeggred <dam17.2021@gmail.com>
2022-06-13 17:33:55 +0200

    [IMP] utm archive origin/campaigns

---
+++ utm_source-campaign_archive_button/models/utm.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class UtmSource(models.Model):
+    _inherit = 'utm.source'
+
+    active = fields.Boolean(string='Active', default=True)
+
+
+class UtmCampaign(models.Model):
+    _inherit = 'utm.campaign'
+
+    active = fields.Boolean(string='Active', default=True)
