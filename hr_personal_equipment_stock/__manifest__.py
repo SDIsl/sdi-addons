218529d0d0f91a59fb5686ab01085c9bfec1994e
Valentin Castravete <valentincastravete@gmail.com>
2022-09-26 18:00:19 +0200

    [FIX]  Flake8

--- hr_personal_equipment_stock/__manifest__.py
+++ hr_personal_equipment_stock/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Hr Personal Equipment Stock",
     "summary": """
-        This addon allows to integrate hr_personal_equipment_request with 
+        This addon allows to integrate hr_personal_equipment_request with
         stock""",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
