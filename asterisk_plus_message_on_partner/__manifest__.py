0236aa3f810b7226272e2deca0e6ae5d892b71fb
Valentin Castravete <valentincastravete@gmail.com>
2022-09-29 12:33:11 +0200

    [ADD] Module for asterisk plus call messages

---
+++ asterisk_plus_message_on_partner/__manifest__.py
@@ -0,0 +1,32 @@
+###############################################################################
+#
+#    SDi Digital Group
+#    Copyright (C) 2021-Today SDi Digital Group <www.sdi.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    'name': 'Asterisk Plus Message On Partner',
+    'summary': '''Replicates the message of a call to the entity\'s
+        partner\'s chatter.''',
+    'author': 'Valentín Castravete Georgian, SDi Soluciones Informáticas',
+    'website': 'https://www.sdi.es/odoo-cloud/',
+    'license': 'AGPL-3',
+    'category': 'Phone',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'asterisk_plus',
+    ],
+}
