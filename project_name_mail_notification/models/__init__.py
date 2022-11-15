ea1197e3d5dec288f4ab7f7cfbf683e5106b01fa
Valentin Castravete <valentincastravete@gmail.com>
2022-11-15 12:48:07 +0100

    [IMP] Project name on mail

--- project_name_mail_notification/models/__init__.py
+++ project_name_mail_notification/models/__init__.py
@@ -1,4 +1,4 @@
 ###############################################################################
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
-from . import mail_mail
+from . import ir_mail_server
