9bff33ee998ecbf48baac9d9c9d262ef8df502e3
Valentin Castravete <valentincastravete@gmail.com>
2022-11-16 15:42:34 +0100

    [FIX] Project name on mail fix

--- project_name_mail_notification/models/ir_mail_server.py
+++ project_name_mail_notification/models/ir_mail_server.py
@@ -12,26 +12,27 @@ class IrMailServer(models.Model):
                     message_id=None, references=None, object_id=False,
                     subtype='plain', headers=None, body_alternative=None,
                     subtype_alternative='plain'):
-        if isinstance(object_id, str):
-            object = object_id.split('-')
-            rec_model = object[1]
-            if email_to and rec_model in ['project.task', 'project.project']:
-                rec_id = object[0]
-                record = self.env[rec_model].search([('id', '=', rec_id)])
-                if record and subject == 'Re: %s' % (record.name):
-                    emails = [email.rsplit(' ', 1)[-1][1:-1]
-                              for email in email_to]
-                    emailed_users = self.env['res.users'].search([
-                        ('partner_id.email', 'in', emails),
-                    ])
-                    if emailed_users:
-                        internal_users = emailed_users.filtered(
-                            lambda user: user.has_group('base.group_user'))
-                        if len(emailed_users.ids) == len(internal_users.ids):
-                            subject = 'Re: %s' % (record.name)
-                            if rec_model == 'project.task':
-                                subject += ' ( %s )' % (record.project_id.name)
-                            subject += ' - %s' % (record.partner_id.name)
+        object = object_id.split('-') if object_id and \
+            isinstance(object_id, str) else []
+        rec_model = object[1] if object and len(object) > 1 else False
+        if rec_model and email_to and \
+                rec_model in ['project.task', 'project.project']:
+            rec_id = object[0]
+            record = self.env[rec_model].search([('id', '=', rec_id)])
+            if record and subject == 'Re: %s' % (record.name):
+                emails = [email.rsplit(' ', 1)[-1][1:-1]
+                          for email in email_to]
+                emailed_users = self.env['res.users'].search([
+                    ('partner_id.email', 'in', emails),
+                ])
+                if emailed_users:
+                    internal_users = emailed_users.filtered(
+                        lambda user: user.has_group('base.group_user'))
+                    if len(emailed_users.ids) == len(internal_users.ids):
+                        subject = 'Re: %s' % (record.name)
+                        if rec_model == 'project.task':
+                            subject += ' ( %s )' % (record.project_id.name)
+                        subject += ' - %s' % (record.partner_id.name)
         return super().build_email(email_from, email_to, subject, body,
                                    email_cc, email_bcc, reply_to, attachments,
                                    message_id, references, object_id, subtype,
