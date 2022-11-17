9770ca84ada6728bdbf3f81d20bdea80f8b5dbce
Jorge Luis Quinteros <quinterosorellana04@gmail.com>
2022-11-17 17:03:17 +0100

    [ADD] mail_template_company_logo (#249)
    * [ADD] mail_template_company_logo
    
    * [IMP] Change logo in all mail notifications templates
    
    * [i18n]
    
    * Apply suggestions from code review
    
    Co-authored-by: Valentín Castravete Georgian <58530982+valentincastravete@users.noreply.github.com>

---
+++ mail_template_company_logo/models/res_company.py
@@ -0,0 +1,12 @@
+###############################################################################
+# For copyright and license notices, see __manifest__.py file in root directory
+###############################################################################
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    url_logo = fields.Char(
+        string='URL Logo',
+    )
