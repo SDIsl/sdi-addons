bccc77f4a5f096e6a8f2adbd053a2c2f95f22466
amartinez <amartinez@sdi.es>
2022-12-15 12:06:28 +0100

    [ADD] partner_activities_smartbutton module

---
+++ partner_activities_smartbutton/models/res_partner.py
@@ -0,0 +1,21 @@
+###############################################################################
+# For copyright and license notices, see __manifest__.py file in root directory
+###############################################################################
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    activities_count = fields.Integer(
+        compute='_compute_activities_count',
+        string='Activities Count',
+    )
+
+    def _compute_activities_count(self):
+        model = self.env['mail.activity']
+        for partner in self:
+            partner.activities_count = model.search_count([
+                ('partner_id', '=', partner.id),
+                ('state', 'not in', ['done'])
+            ])
