93974dbab9b2cd2acb388f66567a4b21308fa3c4
amartinez <amartinez@sdi.es>
2023-02-02 12:14:52 +0100

    [FIX] flake8

--- sale_order_cancelation_date/__init__.py
+++ sale_order_cancelation_date/__init__.py
@@ -10,7 +10,10 @@ import datetime
 def auto_complete_cancelation_date(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     for sale in env['sale.order'].search([('state', '=', 'cancel')]):
-        msg = sale.message_ids.filtered(lambda m: m.tracking_value_ids.filtered(lambda t: t.field == 'state'and t.new_value_char == 'Cancelado'))[:1]
+        msg = sale.message_ids.filtered(
+            lambda m: m.tracking_value_ids.filtered(
+                lambda t: t.field == 'state'and t.new_value_char == 'Cancelado'
+            ))[:1]
         if msg:
             sale.write({
                 'cancelation_date_real': msg.date.date()
