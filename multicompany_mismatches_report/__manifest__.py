613c1d45d592b019b371c5433d0b4800d679d847
Sergio Lop Sanz <slop@sdi.es>
2023-05-22 11:39:40 +0200

    Invoices nos validated with number

--- multicompany_mismatches_report/__manifest__.py
+++ multicompany_mismatches_report/__manifest__.py
@@ -25,7 +25,7 @@
     'website': 'https://www.sdi.es/odoo-cloud/',
     'license': 'AGPL-3',
     'category': 'Accounting',
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'depends': [
         'account',
     ],
