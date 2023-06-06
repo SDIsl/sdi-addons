21815c55e8002c765103d5429633a8075358aa3b
Sergio Lop Sanz <slop@sdi.es>
2023-06-06 11:03:47 +0200

    [FIX] Puto flake

--- l10n_es_ticketbai_global_discount/models/account_invoice.py
+++ l10n_es_ticketbai_global_discount/models/account_invoice.py
@@ -1,6 +1,7 @@
 ###############################################################################
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
+from odoo import models
 from odoo.tools.float_utils import float_round
 
 
