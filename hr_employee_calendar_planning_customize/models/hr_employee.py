90a094479d797998ca5ec715ef4dff030bd4cf6a
Sergio Lop Sanz <slop@sdi.es>
2023-06-07 13:27:29 +0200

    134976 - Introducir nuevo campo "Fecha de contratación" y "Fecha de salida" en Odoo->Empleados (#302)
    * [ADD] Some calculations
    
    * [ADD] Some calculations
    
    * [ADD] Some calculations
    
    * [ADD] Some calculations
    
    * [ADD] Some calculations
    
    * [ADD] Some calculations
    
    * [ADD] Some calculations
    
    * [ADD] Some calculations
    
    * Update hr_employee_calendar_planning_customize/models/hr_employee.py
    
    Co-authored-by: Valentín Castravete Georgian <58530982+valentincastravete@users.noreply.github.com>
    
    * Update hr_employee_calendar_planning_customize/models/hr_employee.py
    
    Co-authored-by: Valentín Castravete Georgian <58530982+valentincastravete@users.noreply.github.com>
    
    * Update hr_employee_calendar_planning_customize/models/hr_employee.py
    
    Co-authored-by: Valentín Castravete Georgian <58530982+valentincastravete@users.noreply.github.com>
    
    * Update hr_employee_calendar_planning_customize/models/hr_employee.py
    
    * Update hr_employee_calendar_planning_customize/models/hr_employee.py
    
    * Update hr_employee_calendar_planning_customize/models/hr_employee.py
    
    * Puto flake8
    
    ---------
    
    Co-authored-by: Valentín Castravete Georgian <58530982+valentincastravete@users.noreply.github.com>

--- hr_employee_calendar_planning_customize/models/hr_employee.py
+++ hr_employee_calendar_planning_customize/models/hr_employee.py
@@ -26,8 +26,28 @@ class HrEmployee(models.Model):
         store=True,
     )
 
-    @api.multi
-    @api.depends('calendar_ids')
+    contract_start = fields.Date(
+        string='Fecha de contratación',
+        copy=False,
+    )
+    contract_end = fields.Date(
+        string='Fecha salida',
+    )
+
+    tenure = fields.Integer(
+        string='Antigüedad (Dias)',
+        compute='_compute_tenure',
+        store=True,
+        depends=['contract_start', 'contract_end'],
+    )
+    tenure_char = fields.Char(
+        string="Antigüedad en la empresa",
+        compute='_show_tenure'
+    )
+
+    @api.depends(
+        'calendar_ids', 'calendar_ids.date_start', 'calendar_ids.date_end'
+    )
     def _compute_antiquity(self):
         today = date.today()
 
@@ -67,7 +87,7 @@ class HrEmployee(models.Model):
                 if line.date_start and line.date_end:
                     start_date = line.date_start.strftime('%Y-%m-%d')
                     end_date = line.date_end.strftime('%Y-%m-%d')
-                    if actual_date >= start_date and actual_date <= end_date:
+                    if start_date <= actual_date <= end_date:
                         rec.actual_calendar_id = line.calendar_id
                         break
 
@@ -80,7 +100,7 @@ class HrEmployee(models.Model):
             for line in record.calendar_ids:
                 start_date = line.date_start.strftime('%Y-%m-%d')
                 end_date = line.date_end.strftime('%Y-%m-%d')
-                if actual_date >= start_date and actual_date <= end_date:
+                if start_date <= actual_date <= end_date:
                     record.update({'actual_calendar_id': line.calendar_id})
                     break
 
@@ -135,3 +155,21 @@ class HrEmployee(models.Model):
                 if line.date_start and line.date_end:
                     line.date_start = line.date_start - relativedelta(years=1)
                     line.date_end = line.date_end - relativedelta(years=1)
+
+    def _compute_inicio_contrato(self):
+        for rec in self.filtered(lambda a: a.calendar_ids):
+            inicio = min(rec.calendar_ids.mapped('date_start')) or False
+            rec.contract_start = inicio
+
+    @api.depends('contract_start', 'contract_end')
+    def _compute_tenure(self):
+        for rec in self.filtered(lambda x: x.contract_start):
+            fin = rec.contract_end or date.today()
+            rec.tenure = (fin - rec.contract_start).days
+
+    def _show_tenure(self):
+        for rec in self:
+            y = str(rec.tenure // 365).zfill(2)
+            m = str((rec.tenure % 365) // 30).zfill(2)
+            d = str((rec.tenure % 365) % 30).zfill(2)
+            rec.tenure_char = "{} años, {} meses, {} días.".format(y, m, d)
