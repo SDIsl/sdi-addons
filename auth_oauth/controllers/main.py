df1d66f260fba7624cfe2f408d870507c647b576
Fernando La Chica (Sidoo SDi. Digital Group) <flachica@sidoo.es>
2025-07-10 10:08:03 +0200

    Forzar ssl para dominios distintos a localhost

--- auth_oauth/controllers/main.py
+++ auth_oauth/controllers/main.py
@@ -58,6 +58,8 @@ class OAuthLogin(Home):
             providers = []
         for provider in providers:
             return_url = request.httprequest.url_root + 'auth_oauth/signin'
+            if not return_url.startswith('http://localhost:'):
+                return_url = return_url.replace('http://', 'https://')
             state = self.get_state(provider)
             params = dict(
                 response_type='token',
