f941073068f07e7aa1ad8bccc2e5a1366abf6ddb
Sergio Lop Sanz <slop@sdi.es>
2025-10-23 00:20:05 +0200

    Mensaje de reclamación

---
+++ sdi_account_payment_return/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Mejoras Account Payment Return',
+    'version': '12.0.1.0.0',
+    'depends': ['account_payment_return'],
+    'author': 'Sergio Lop Sanz <SDi Digital Group>',
+    'category': 'Accounting',
+    'description': 'Mejoras y extensiones para payment return',
+    'data': [
+        'data/mail_templates.xml',
+        'views/payment_return_views.xml',
+    ],
+    'installable': True,
+}
